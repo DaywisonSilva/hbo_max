46ceb9522ec1aa24095ee4df4fb18d0c928639bf
matheus <matheusbahia10@gmail.com>
2022-06-15 21:37:22 -0300

    feat: create Themes stitches

--- src/themes/default.styles.ts
+++ src/themes/default.styles.ts
@@ -4,19 +4,13 @@ export const { getCssText } = createStitches({
   theme: {
     colors: {
       black: "#000",
-      white: "f2f2f2",
+      white: "#f2f2f2",
     },
     space: {
       defaultMargin: "20px",
     },
-    borderStyles: {
-      defaultBorderRadius: "20px",
-    },
     radii: {
       defaultBorderRadius: "10px",
-    },
-    filters: {
-      defaultFilter: "blur(20px)",
     },
   },
 });
