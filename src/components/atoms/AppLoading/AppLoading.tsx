073816296e846c7ce1bce996af262ae24a6766d4
DaywisonSilva <daywisonsilvasou@gmail.com>
2022-09-22 14:21:20 -0300

    build: resolve isolatedModules

--- src/components/atoms/AppLoading/AppLoading.tsx
+++ src/components/atoms/AppLoading/AppLoading.tsx
@@ -7,7 +7,6 @@ function AppLoading() {
         width='133'
         height='22'
         viewBox='0 0 133 22'
-        fill='none'
         xmlns='http://www.w3.org/2000/svg'
       >
         <path
